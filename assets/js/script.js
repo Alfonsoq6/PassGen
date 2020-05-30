0e6c9ca45dc87bd889a3d17cf1255069f69cf6ce
Alfonso Quevedo <alfonsoq6@gmail.com>
2020-05-30 12:23:27 -0700

    missed uppercase in an if causing it to only use in uppercase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var generateBtn = document.querySelector("#generate");
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w","x", "y", "z",];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s", "t", "u", "v", "w","x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
@@ -100,7 +100,7 @@ function generatePassword() {
     chosenCharOptions.push(getRandomItem(lowerCase));
   }
   if (passwordComponents.upperCaseChoice) {
-    chosenArrays = chosenArrays.concat();
+    chosenArrays = chosenArrays.concat(upperCase);
     chosenCharOptions.push(getRandomItem(upperCase));
   }
   if (passwordComponents.numArrayChoice) {
